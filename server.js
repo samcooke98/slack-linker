eb823950a70b10208470acbc60efb61869a2057e
Samuel Cooke <samuel.cooke@vivant.com.au>
2016-11-04 13:24:06 +1100

    First Commit, Testing Outgoing-webhook

---
+++ server.js
@@ -0,0 +1,19 @@
+var slackbots = require('slackbots');
+var express = require('express');
+
+var app = express();
+
+/* Idea List - 
+    /idea A sentence or two to remind you what your idea is
+    Will be added to a shared google doc, with your name; Will also be announced in the channel that you /idea in
+*/
+
+app.get('/addIdea', function (req, res ) { 
+    console.log(req);
+    if(req.token == "35aJgYpwkeYUbeqzhJhuV30U") { 
+        //Get the text
+        
+    } else {
+        res.send("Invalid Token");
+    }
+})
